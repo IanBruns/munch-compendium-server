7dc7c56ebde3a7402a3054da76f8e1021d4752f0
Ian Bruns <ian@rbruns.com>
2021-04-15 00:05:44 -0600

    added some sample arrays

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -18,6 +18,53 @@ function cleanTables(db) {
     )
 }
 
+function makeBrandsArray() {
+    return [
+        {
+            id: 1,
+            brand_name: `Wendy's`
+        },
+        {
+            id: 2,
+            brand_name: `Carl's Jr`
+        }
+    ];
+}
+
+function makePostsArray() {
+    return [
+        {
+            id: 1,
+            brand_id: 1,
+            episode_number: 1,
+            episode_title: 'Title for Episode 1',
+            episode_link: 'Link for Episode 1',
+            campaign: 'Brand for campaign 1',
+            pr_link: 'Link for campaign 1'
+        },
+        {
+            id: 2,
+            brand_id: 2,
+            episode_number: 2,
+            episode_title: 'Title for Episode 2',
+            episode_link: 'Link for Episode 2',
+            campaign: 'Brand for campaign 2',
+            pr_link: null
+        },
+        {
+            id: 3,
+            brand_id: 3,
+            episode_number: 3,
+            episode_title: 'Title for Episode 3',
+            episode_link: 'Link for Episode 3',
+            campaign: 'Brand for campaign 3',
+            pr_link: 'Link for campaign 3'
+        },
+    ]
+}
+
 module.exports = {
-    cleanTables
+    cleanTables,
+    makeBrandsArray,
+    makePostsArray
 }
