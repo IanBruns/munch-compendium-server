435699d3778081b560a425955aa09610f9f7d735
Ian Bruns <ian@rbruns.com>
2021-04-19 10:22:29 -0600

    GET works

--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const PostsService = require('./posts-service');
 
 const postsRouter = express.Router();
 
 postsRouter.route('/')
     .get(async (req, res, next) => {
-        return res.status(200).json([]);
+        const posts = await PostsService.getPosts(req.app.get('db'));
+        return res.status(200).json(posts);
     });
 
 module.exports = postsRouter;
