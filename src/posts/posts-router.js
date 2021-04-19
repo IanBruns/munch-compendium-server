7e1bd869c52fabd2bfe92ba07b95c4d37a197dfd
Ian Bruns <ian@rbruns.com>
2021-04-19 14:46:10 -0600

    added try/catch

--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -5,14 +5,22 @@ const postsRouter = express.Router();
 
 postsRouter.route('/')
     .get(async (req, res, next) => {
-        const posts = await PostsService.getPosts(req.app.get('db'));
-        return res.status(200).json(posts);
+        try {
+            const posts = await PostsService.getPosts(req.app.get('db'));
+            return res.status(200).json(posts);
+        } catch (err) {
+            next(err);
+        }
     });
 
 postsRouter.route('/brands/:brand_id')
     .get(async (req, res, next) => {
-        const posts = await PostsService.getBrandPosts(req.app.get('db'), req.params.brand_id);
-        return res.status(200).json(posts);
+        try {
+            const posts = await PostsService.getBrandPosts(req.app.get('db'), req.params.brand_id);
+            return res.status(200).json(posts);
+        } catch (err) {
+            next(err);
+        }
     });
 
 module.exports = postsRouter;
