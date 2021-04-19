25de2c187583993fb40cbf55488d78c746c5e5f4
Ian Bruns <ian@rbruns.com>
2021-04-19 14:37:09 -0600

    starting brands tests

---
+++ test/brands.spec.js
@@ -0,0 +1,23 @@
+const knex = require('knex');
+const supertest = require('supertest');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe('brands endpoints', () => {
+    let db;
+
+    before(`Make knex instance`, () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DATABASE_URL,
+        });
+
+        app.set('db', db);
+    });
+
+    after('disconnect from db', () => db.destroy());
+
+    before('cleanup', () => helpers.cleanTables(db));
+
+    afterEach('cleanup', () => helpers.cleanTables(db));
+});
